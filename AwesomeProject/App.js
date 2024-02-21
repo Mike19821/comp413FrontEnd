b225da35e57823d7146589e9793856e4195a0334
Mike <Mike@macbook-pro-47.dyndns.rice.edu>
2024-02-21 13:38:15 -0600

    ss

--- AwesomeProject/App.js
+++ AwesomeProject/App.js
@@ -1,12 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import * as React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+
+const Stack = createNativeStackNavigator();
+
+const HomeScreen = ({navigation}) => {
+  return (
+    <View style={styles.container}>
+      <Text>OK</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+};
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          component={HomeScreen}
+          options={{title: 'Welcome'}}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
